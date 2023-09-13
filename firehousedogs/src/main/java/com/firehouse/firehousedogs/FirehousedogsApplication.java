2199d5967ba240d5b9f2d1507b7857b2dc1c4dbf
thomas-shaw0 <144106736+thomas-shaw0@users.noreply.github.com>
2023-09-13 10:46:03 +0000

    all

---
+++ firehousedogs/src/main/java/com/firehouse/firehousedogs/FirehousedogsApplication.java
@@ -0,0 +1,13 @@
+package com.firehouse.firehousedogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirehousedogsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirehousedogsApplication.class, args);
+	}
+
+}
